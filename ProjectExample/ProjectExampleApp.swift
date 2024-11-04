061acc51e64d00f86f297c8b00bce456dedfba96
aazzouz.fatima.ezzahra <aazzouz.fatima.ezzahra@gmail.com>
2024-11-04 15:18:55 +0100

    Initial Commit

---
+++ ProjectExample/ProjectExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectExampleApp.swift
+//  ProjectExample
+//
+//  Created by aazzouz fatima ezzahra on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
